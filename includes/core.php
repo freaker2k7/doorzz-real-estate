9f5b21fe363c55de681dfc12868668a1df99e856
freaker2k7 <thenetfreaker@gmail.om>
2018-01-15 00:08:03 +0200

    Simplified default template

--- includes/core.php
+++ includes/core.php
@@ -18,6 +18,19 @@ final class DOORZZ_REAL_ESTATE {
 	private const CDN_URL = 'https://cdn.doorzz.com/';
 	private const LINK_TO_HID = 'https://dorz.me/id/';
 	
+	private const DOORZZ_REAL_ESTATE_DEFAULT_TEMPLATE = 
+'<a style="display: inline-block; width: 250px; height: 100%; vertical-align: top; margin: 0 10px; text-decoration: none;" href="{{LINK_TO_HID}}" target="_blank">
+	<h2 style="font-size: 1rem;">{title}</h2>
+	<div style="width: 100%; height: 100px; overflow: hidden; background: #efefef; position: relative;">
+		<img src="{img}" alt="{free_text}" title="{free_text}" style="width: 100%; position: absolute; margin: auto; top: 0; left: 0; right: 0; bottom: 0;" />
+	</div>
+	<h2 style="text-transform: capitalize; font-size: 1rem; padding-top: 0.5rem; margin-bottom: 0;">
+		<i class="local-icons {type} {subtype}"></i> {subtype} for {type}<br>
+		${price}/{period}<br>
+		{location}
+	</h4>
+</a>';
+	
 	private static $PERIODS = array(
 		'' => 'Month',
 		2592000 => 'Month',
@@ -148,24 +161,13 @@ final class DOORZZ_REAL_ESTATE {
 		global $wpdb;
 		
 		if (!$data) {
-			require_once(DOORZZ_REAL_ESTATE_PLUGIN_DIR . 'views/cards/default.php');
-			global $DOORZZ_REAL_ESTATE_DEFAULT_TEMPLATE;
-			
-			$data = $DOORZZ_REAL_ESTATE_DEFAULT_TEMPLATE;
+			$data = self::DOORZZ_REAL_ESTATE_DEFAULT_TEMPLATE;
 		}
 		
 		$wpdb->replace( 
-			self::_get_table_name($wpdb), 
-			array( 
-				'name' => 'default', 
-				'time' => current_time( 'mysql' ), 
-				'data' => $data
-			), 
-			array( 
-				'%s',
-				'%s', 
-				'%s' 
-			) 
+			self::_get_table_name($wpdb),
+			array('name' => 'default', 'time' => current_time( 'mysql' ), 'data' => $data),
+			array('%s', '%s', '%s')
 		);
 		
 		wp_cache_set('real_estate_template', $data);
